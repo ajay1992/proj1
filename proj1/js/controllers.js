6263be9105352546860cd79bfd5a5cf77fac072e
ajay1992 <ajayvarma656@gmail.com>
2016-02-13 06:10:30 +0530

    new

--- proj1/js/controllers.js
+++ proj1/js/controllers.js
@@ -1,10 +1,42 @@
 angular.module('app.controllers', [])
   
 .controller('loginCtrl', function($scope) {
-
+   var data={email:"",
+   password:""};
+   $scope.data= data;
+  $scope.loginEmail = function(){
+ 
+  var ref = new Firebase("https://attend1234.firebaseio.com");
+ 
+  ref.authWithPassword({
+    email    : $scope.data.email,
+    password : $scope.data.password
+  }, function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log("Authenticated successfully with payload:", authData);
+    }
+  });
+ 
+};
 })
    
 .controller('signupCtrl', function($scope) {
+    var ref = new Firebase("https://attend1234.firebaseio.com");
+    var data={email:"",
+   password:""};
+   $scope.data= data;
+ref.createUser({
+ email    : $scope.data.email,
+ password : $scope.data.password
+}, function(error, userData) {
+  if (error) {
+    console.log("Error creating user:", error);
+  } else {
+    console.log("Successfully created user account with uid:", userData.uid);
+  }
+});
 
 })
  
